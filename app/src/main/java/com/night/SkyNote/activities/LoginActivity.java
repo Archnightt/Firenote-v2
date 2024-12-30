3904070dc18229e0558412f5a69a0deebceb45e8
archnightt <kolarkar.harsh@gmail.com>
2024-12-31 00:38:20 +0530

    Major Update - Firestore

--- app/src/main/java/com/night/SkyNote/activities/LoginActivity.java
+++ app/src/main/java/com/night/SkyNote/activities/LoginActivity.java
@@ -131,7 +131,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     // Navigate to MainActivity
-    private void navigateToMainActivity() {
+    public void navigateToMainActivity() {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
